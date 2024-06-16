4e8b6eaf0f5e65975d3aac00f1f2728170d9eb78
Naga Manohar Yelubandi <61689114+Naga-Manohar-Y@users.noreply.github.com>
2024-06-16 15:51:13 +0000

    .

---
+++ GitHub-SDks/Ruby/main.rb
@@ -0,0 +1,10 @@
+require 'octokit'
+
+client = Octokit::Client.new(access_token: ENV['GH_TOKEN'])
+
+# Create the new branch
+client.create_ref(
+"Naga-Manohar-Y/Github-Examples", 
+"heads/sdks", 
+"3a652b86905ca4e7888f4898e1b6f5d957b2c926"
+)
